e4bcf68c0e18994bd5c7b4886af07bdd450f2891
tarunkatariaonline <tarunsoftsol@gmail.com>
2024-04-01 00:07:43 +0530

    style code editor component || home component style

--- frontend codevilla/src/Components/CodeRoom.jsx
+++ frontend codevilla/src/Components/CodeRoom.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { io } from 'socket.io-client';
 import { useParams,useSearchParams } from 'react-router-dom';
-import { useCallback } from 'react';
+
+import Avatar from 'react-avatar';
 import {  toast } from 'react-toastify';
 
 function CodeRoom() {
@@ -68,23 +69,33 @@ function CodeRoom() {
 
       },[])
   return (
-    <div>
-        <div>
-            {users.map((user,index)=>{
-             return <li key={index} >{user.username}</li>
-            })}
-        </div>
-        <div>
-          {codeText}
-        </div>
-        <div>
-            <textarea value={codeText} onChange={handlerCodewriter} name="" id="" cols="30" rows="10"></textarea>
+    <div className='  -z-10 w-full h-[100vh] text-slate-400 bg-slate-900  flex justify-center items-center   flex-col  p-10 '>
+      <img className=' absolute  -z-0 top-0 right-0 backdrop-blur-lg' src='https://tailwindcss.com/_next/static/media/docs-dark@30.1a9f8cbf.avif'/>
+
+      <div className=' w-full h-full z-30 flex'>
+
+        <div className='    w-72 h-full bg-gray-500/50 rounded-lg p-2 justify-center items-start'>
+         
+        <div className=' '>
+        <div className=' m-1  text-md text-gray-300 font-bold '>
+          Connected Users :
         </div>
 
-        <div>
-        <button style={{backgroundColor:"green",color:"white"}}>Copy Room id</button>
-            <button style={{backgroundColor:"blue",color:"white"}}>leave</button>
+        {users.map((user,index)=>{
+         return  <Avatar key={index} name={user.username} size='45' className='  rounded-lg  m-1 '  textSizeRatio={2.5}  />
+        })}
+       
+       
         </div>
+        
+        </div>
+
+        <div className='  ml-3 w-full h-full  rounded-lg'>
+         <textarea value={codeText} placeholder=' Code here' onChange={handlerCodewriter} className=' w-full h-full bg-gray-500/50 rounded-lg  text-white ' name="" id="" cols="30" rows="10"></textarea>
+        </div>
+
+      </div>
+       
     </div>
   )
 }
